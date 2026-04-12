d439af22a54139117a05b4561c47a2024c0418a7
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-12 17:56:41 +0800

    Add files via upload

--- src/app/pages/client/home/useHomeRooms.ts
+++ src/app/pages/client/home/useHomeRooms.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { useAtomValue } from 'jotai';
 import { useMatrixClient } from '../../../hooks/useMatrixClient';
+import { useFavoritesRoomId } from '../../../hooks/useFavoritesRoom';
 import { mDirectAtom } from '../../../state/mDirectList';
 import { roomToParentsAtom } from '../../../state/room/roomToParents';
 import { allRoomsAtom } from '../../../state/room-list/roomList';
@@ -9,6 +11,11 @@ export const useHomeRooms = () => {
   const mx = useMatrixClient();
   const mDirects = useAtomValue(mDirectAtom);
   const roomToParents = useAtomValue(roomToParentsAtom);
+  const favoritesRoomId = useFavoritesRoomId();
   const rooms = useOrphanRooms(mx, allRoomsAtom, mDirects, roomToParents);
-  return rooms;
+
+  return useMemo(
+    () => rooms.filter((roomId) => roomId !== favoritesRoomId),
+    [rooms, favoritesRoomId]
+  );
 };
