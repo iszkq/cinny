155cb15433e2904807c8aecc7ef279879b550c35
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-12 17:55:08 +0800

    Add files via upload

--- src/app/pages/client/direct/useDirectRooms.ts
+++ src/app/pages/client/direct/useDirectRooms.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { useAtomValue } from 'jotai';
 import { useMatrixClient } from '../../../hooks/useMatrixClient';
+import { useFavoritesRoomId } from '../../../hooks/useFavoritesRoom';
 import { mDirectAtom } from '../../../state/mDirectList';
 import { allRoomsAtom } from '../../../state/room-list/roomList';
 import { useDirects } from '../../../state/hooks/roomList';
@@ -7,6 +9,11 @@ import { useDirects } from '../../../state/hooks/roomList';
 export const useDirectRooms = () => {
   const mx = useMatrixClient();
   const mDirects = useAtomValue(mDirectAtom);
+  const favoritesRoomId = useFavoritesRoomId();
   const directs = useDirects(mx, allRoomsAtom, mDirects);
-  return directs;
+
+  return useMemo(
+    () => directs.filter((roomId) => roomId !== favoritesRoomId),
+    [directs, favoritesRoomId]
+  );
 };
