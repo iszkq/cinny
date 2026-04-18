173289c21435dcd27fde6daba891fe7c380fe0b2
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-18 12:51:30 +0800

    Add files via upload

---
+++ src/app/components/emoji-board/components/components/Item.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Box, Icon, Icons } from 'folds';
+import { MatrixClient } from 'matrix-js-sdk';
+import { IImageInfo } from '../../../../types/matrix/common';
+import { EmojiItemInfo, EmojiType } from '../types';
+import * as css from './styles.css';
+import { PackImageReader } from '../../../plugins/custom-emoji';
+import { IEmoji } from '../../../plugins/emoji';
+import { mxcUrlToHttp } from '../../../utils/matrix';
+import { useStableMediaUrl } from './useStableMediaUrl';
+
+export const getEmojiItemInfo = (element: Element): EmojiItemInfo | undefined => {
+  const label = element.getAttribute('title');
+  const type = element.getAttribute('data-emoji-type') as EmojiType | undefined;
+  const data = element.getAttribute('data-emoji-data');
+  const shortcode = element.getAttribute('data-emoji-shortcode');
+  const infoStr = element.getAttribute('data-emoji-info');
+
+  let info: IImageInfo | undefined;
+  if (infoStr) {
+    try {
+      const parsedInfo = JSON.parse(infoStr);
+      if (parsedInfo && typeof parsedInfo === 'object') {
+        info = parsedInfo as IImageInfo;
+      }
+    } catch {
+      info = undefined;
+    }
+  }
+
+  if (type && data && shortcode && label)
+    return {
+      type,
+      data,
+      shortcode,
+      label,
+      info,
+    };
+  return undefined;
+};
+
+type EmojiItemProps = {
+  emoji: IEmoji;
+};
+export function EmojiItem({ emoji }: EmojiItemProps) {
+  return (
+    <Box
+      as="button"
+      type="button"
+      alignItems="Center"
+      justifyContent="Center"
+      className={css.EmojiItem}
+      title={emoji.label}
+      aria-label={`${emoji.label} emoji`}
+      data-emoji-type={EmojiType.Emoji}
+      data-emoji-data={emoji.unicode}
+      data-emoji-shortcode={emoji.shortcode}
+    >
+      {emoji.unicode}
+    </Box>
+  );
+}
+
+type CustomEmojiItemProps = {
+  mx: MatrixClient;
+  useAuthentication?: boolean;
+  image: PackImageReader;
+};
+export function CustomEmojiItem({ mx, useAuthentication, image }: CustomEmojiItemProps) {
+  const mediaUrl = mxcUrlToHttp(mx, image.url, useAuthentication) ?? '';
+  const { displayUrl, hasFailed, handleLoad, handleError } = useStableMediaUrl(mediaUrl);
+
+  return (
+    <Box
+      as="button"
+      type="button"
+      alignItems="Center"
+      justifyContent="Center"
+      className={css.EmojiItem}
+      title={image.body || image.shortcode}
+      aria-label={`${image.body || image.shortcode} emoji`}
+      data-emoji-type={EmojiType.CustomEmoji}
+      data-emoji-data={image.url}
+      data-emoji-shortcode={image.shortcode}
+    >
+      {displayUrl && !hasFailed ? (
+        <img
+          loading="eager"
+          decoding="async"
+          className={css.CustomEmojiImg}
+          alt=""
+          src={displayUrl}
+          onLoad={handleLoad}
+          onError={handleError}
+        />
+      ) : (
+        <Box className={css.CustomEmojiFallback}>
+          <Icon src={Icons.Photo} />
+        </Box>
+      )}
+    </Box>
+  );
+}
+
+type StickerItemProps = {
+  mx: MatrixClient;
+  useAuthentication?: boolean;
+  image: PackImageReader;
+};
+
+export function StickerItem({ mx, useAuthentication, image }: StickerItemProps) {
+  const mediaUrl = mxcUrlToHttp(mx, image.url, useAuthentication) ?? '';
+  const { displayUrl, hasFailed, handleLoad, handleError } = useStableMediaUrl(mediaUrl);
+
+  return (
+    <Box
+      as="button"
+      type="button"
+      alignItems="Center"
+      justifyContent="Center"
+      className={css.StickerItem}
+      title={image.body || image.shortcode}
+      aria-label={`${image.body || image.shortcode} emoji`}
+      data-emoji-type={EmojiType.Sticker}
+      data-emoji-data={image.url}
+      data-emoji-shortcode={image.shortcode}
+      data-emoji-info={image.info ? JSON.stringify(image.info) : undefined}
+    >
+      {displayUrl && !hasFailed ? (
+        <img
+          loading="eager"
+          decoding="async"
+          className={css.StickerImg}
+          alt=""
+          src={displayUrl}
+          onLoad={handleLoad}
+          onError={handleError}
+        />
+      ) : (
+        <Box className={css.StickerFallback}>
+          <Icon src={Icons.Photo} />
+        </Box>
+      )}
+    </Box>
+  );
+}
---
+++ src/app/components/emoji-board/components/components/Preview.tsx
@@ -0,0 +1,65 @@
+import { Box, Icon, Icons, Text } from 'folds';
+import React from 'react';
+import { Atom, atom, useAtomValue } from 'jotai';
+import * as css from './styles.css';
+import { useMatrixClient } from '../../../hooks/useMatrixClient';
+import { useMediaAuthentication } from '../../../hooks/useMediaAuthentication';
+import { mxcUrlToHttp } from '../../../utils/matrix';
+import { useStableMediaUrl } from './useStableMediaUrl';
+
+export type PreviewData = {
+  key: string;
+  shortcode: string;
+};
+
+export const createPreviewDataAtom = (initial?: PreviewData) =>
+  atom<PreviewData | undefined>(initial);
+
+type PreviewProps = {
+  previewAtom: Atom<PreviewData | undefined>;
+};
+export function Preview({ previewAtom }: PreviewProps) {
+  const mx = useMatrixClient();
+  const useAuthentication = useMediaAuthentication();
+
+  const { key, shortcode } = useAtomValue(previewAtom) ?? {};
+  const mediaUrl =
+    key && key.startsWith('mxc://') ? (mxcUrlToHttp(mx, key, useAuthentication) ?? key) : undefined;
+  const { displayUrl, hasFailed, handleLoad, handleError } = useStableMediaUrl(mediaUrl);
+
+  if (!shortcode) return null;
+
+  return (
+    <Box shrink="No" className={css.Preview} gap="300" alignItems="Center">
+      {key && (
+        <Box
+          display="InlineFlex"
+          className={css.PreviewEmoji}
+          alignItems="Center"
+          justifyContent="Center"
+        >
+          {key.startsWith('mxc://') ? (
+            displayUrl && !hasFailed ? (
+              <img
+                className={css.PreviewImg}
+                src={displayUrl}
+                alt=""
+                onLoad={handleLoad}
+                onError={handleError}
+              />
+            ) : (
+              <Box className={css.PreviewFallback}>
+                <Icon src={Icons.Photo} size="100" />
+              </Box>
+            )
+          ) : (
+            key
+          )}
+        </Box>
+      )}
+      <Text size="H5" truncate>
+        :{shortcode}:
+      </Text>
+    </Box>
+  );
+}
