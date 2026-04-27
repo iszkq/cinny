a5b79f08744dda5b1018a6c929004b7136bc9811
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-27 10:17:59 +0800

    Add files via upload

--- src/app/pages/client/SyncStatus.tsx
+++ src/app/pages/client/SyncStatus.tsx
@@ -44,7 +44,7 @@ export function SyncStatus({ mx }: SyncStatusProps) {
           alignItems="Center"
           justifyContent="Center"
         >
-          <Text size="L400">Connecting...</Text>
+          <Text size="L400">正在连接...</Text>
         </Box>
         <Line variant="Success" size="300" />
       </Box>
@@ -60,7 +60,7 @@ export function SyncStatus({ mx }: SyncStatusProps) {
           alignItems="Center"
           justifyContent="Center"
         >
-          <Text size="L400">Connection Lost! Reconnecting...</Text>
+          <Text size="L400">连接已断开，正在重连...</Text>
         </Box>
         <Line variant="Warning" size="300" />
       </Box>
@@ -76,7 +76,7 @@ export function SyncStatus({ mx }: SyncStatusProps) {
           alignItems="Center"
           justifyContent="Center"
         >
-          <Text size="L400">Connection Lost!</Text>
+          <Text size="L400">连接已断开</Text>
         </Box>
         <Line variant="Critical" size="300" />
       </Box>
