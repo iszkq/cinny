ca3068d11df7bebaf4a548b82aeab08002bc1473
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-27 15:15:32 +0800

    Add files via upload

---
+++ src/app/constants/branding.ts
@@ -0,0 +1,6 @@
+import AppLogo from '../../../public/res/logo.png';
+
+export const APP_DISPLAY_NAME = '星火';
+export const APP_TAGLINE = '星星之火，可以燎原';
+export const APP_WEB_DEVICE_NAME = '星火 Web';
+export const APP_LOGO_URL = AppLogo;
