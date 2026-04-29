892437de9be376bc6fe80cf5dad2969b4ebfd6f3
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-29 13:31:27 +0800

    Add files via upload

--- src/app/pages/client/ClientLayout.tsx
+++ src/app/pages/client/ClientLayout.tsx
@@ -1,23 +1,11 @@
 import React, { ReactNode } from 'react';
 import { Box } from 'folds';
-import { ScreenSize, useScreenSizeContext } from '../../hooks/useScreenSize';
 
 type ClientLayoutProps = {
   nav: ReactNode;
   children: ReactNode;
 };
 export function ClientLayout({ nav, children }: ClientLayoutProps) {
-  const screenSize = useScreenSizeContext();
-
-  if (screenSize === ScreenSize.Mobile) {
-    return (
-      <Box grow="Yes" direction="Column">
-        <Box shrink="No">{nav}</Box>
-        <Box grow="Yes">{children}</Box>
-      </Box>
-    );
-  }
-
   return (
     <Box grow="Yes">
       <Box shrink="No">{nav}</Box>
--- src/app/pages/client/SidebarNav.tsx
+++ src/app/pages/client/SidebarNav.tsx
@@ -20,23 +20,15 @@ import {
   FavoritesTab,
 } from './sidebar';
 import { CreateTab } from './sidebar/CreateTab';
-import { ScreenSize, useScreenSizeContext } from '../../hooks/useScreenSize';
 
 export function SidebarNav() {
   const scrollRef = useRef<HTMLDivElement>(null);
-  const screenSize = useScreenSizeContext();
-  const mobile = screenSize === ScreenSize.Mobile;
 
   return (
     <Sidebar>
       <SidebarContent
         scrollable={
-          <Scroll
-            ref={scrollRef}
-            variant="Background"
-            size="0"
-            direction={mobile ? 'Horizontal' : 'Vertical'}
-          >
+          <Scroll ref={scrollRef} variant="Background" size="0">
             <SidebarStack>
               <HomeTab />
               <DirectTab />
