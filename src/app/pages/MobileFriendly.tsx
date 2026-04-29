b36b3408c2422387001ce989ce17ad6b1dc1652c
iszkq <167393945+iszkq@users.noreply.github.com>
2026-04-30 00:10:32 +0800

    Add files via upload

--- src/app/pages/MobileFriendly.tsx
+++ src/app/pages/MobileFriendly.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
+import { useAtomValue } from 'jotai';
 import { useMatch } from 'react-router-dom';
-import { isCompactScreenSize, useScreenSizeContext } from '../hooks/useScreenSize';
+import { isCompactScreenSize, ScreenSize, useScreenSizeContext } from '../hooks/useScreenSize';
+import { desktopPageNavCollapsedAtom } from '../state/desktopPageNav';
 
 type MobileFriendlyClientNavProps = {
   children: ReactNode;
@@ -20,11 +22,16 @@ type MobileFriendlyPageNavProps = {
 };
 export function MobileFriendlyPageNav({ path, children }: MobileFriendlyPageNavProps) {
   const screenSize = useScreenSizeContext();
+  const desktopPageNavCollapsed = useAtomValue(desktopPageNavCollapsedAtom);
   const exactPath = useMatch({
     path,
     caseSensitive: true,
     end: true,
   });
+
+  if (screenSize === ScreenSize.Desktop && desktopPageNavCollapsed) {
+    return null;
+  }
 
   if (isCompactScreenSize(screenSize) && !exactPath) {
     return null;
