6895c80e87978adc769ebdeecfb4b99356a47ae2
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-18 23:26:44 +0800

    Add files via upload

--- src/ext.d.ts
+++ src/ext.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare const __APP_VERSION__: string;
+
 interface ImportMetaEnv {
   readonly VITE_HASH_ROUTER_ENABLED?: string;
   readonly VITE_HASH_ROUTER_BASENAME?: string;
