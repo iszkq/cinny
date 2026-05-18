d0e98d45d9b74fbca8d389ca2902569030cca3a6
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-18 16:12:06 +0800

    Add files via upload

--- src/app/features/room/RoomViewHeader.css.ts
+++ src/app/features/room/RoomViewHeader.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { config } from 'folds';
 
 export const HeaderTopic = style({
+  opacity: config.opacity.P400,
   ':hover': {
     cursor: 'pointer',
     opacity: config.opacity.P500,
