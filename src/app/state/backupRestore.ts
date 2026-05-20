87190d61ee04b9c3e3bde9815f810b8f6a32750f
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-21 01:31:12 +0800

    Add files via upload

--- src/app/state/backupRestore.ts
+++ src/app/state/backupRestore.ts
@@ -32,6 +32,13 @@ const baseBackupRestoreProgressAtom = atom<IBackupProgress>({
   status: BackupProgressStatus.Idle,
 });
 
+export const setBackupRestoreProgressAtom = atom(
+  null,
+  (_get, set, progress: IBackupProgress) => {
+    set(baseBackupRestoreProgressAtom, progress);
+  }
+);
+
 export const backupRestoreProgressAtom = atom<
   IBackupProgress,
   [ImportRoomKeyProgressData],
