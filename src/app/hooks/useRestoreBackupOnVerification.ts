786dead6fa59b029ee17511f67dce30743f8b66a
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-21 14:37:12 +0800

    Add files via upload

--- src/app/hooks/useRestoreBackupOnVerification.ts
+++ src/app/hooks/useRestoreBackupOnVerification.ts
@@ -1,55 +1,24 @@
 import { useSetAtom } from 'jotai';
-import { useCallback, useEffect, useRef } from 'react';
-import {
-  BackupProgressStatus,
-  backupRestoreProgressAtom,
-  setBackupRestoreProgressAtom,
-} from '../state/backupRestore';
+import { useCallback } from 'react';
+import { backupRestoreProgressAtom } from '../state/backupRestore';
 import { useMatrixClient } from './useMatrixClient';
 import { useKeyBackupDecryptionKeyCached } from './useKeyBackup';
-import { restoreKeyBackupAndDecrypt } from '../utils/keyBackup';
 
 export const useRestoreBackupOnVerification = () => {
   const setRestoreProgress = useSetAtom(backupRestoreProgressAtom);
-  const setRestoreProgressState = useSetAtom(setBackupRestoreProgressAtom);
 
   const mx = useMatrixClient();
-  const restorePromiseRef = useRef<Promise<void>>();
-
-  const attemptRestore = useCallback(() => {
-    if (restorePromiseRef.current) {
-      return restorePromiseRef.current;
-    }
-
-    restorePromiseRef.current = restoreKeyBackupAndDecrypt(mx, {
-      progressCallback(progress) {
-        setRestoreProgress(progress);
-      },
-    })
-      .then(() => {
-        setRestoreProgressState({
-          status: BackupProgressStatus.Done,
-        });
-      })
-      .catch(() => {
-        setRestoreProgressState({
-          status: BackupProgressStatus.Idle,
-        });
-      })
-      .finally(() => {
-        restorePromiseRef.current = undefined;
-      });
-
-    return restorePromiseRef.current;
-  }, [mx, setRestoreProgress, setRestoreProgressState]);
-
-  useEffect(() => {
-    void attemptRestore();
-  }, [attemptRestore]);
 
   useKeyBackupDecryptionKeyCached(
     useCallback(() => {
-      void attemptRestore();
-    }, [attemptRestore])
+      const crypto = mx.getCrypto();
+      if (!crypto) return;
+
+      crypto.restoreKeyBackup({
+        progressCallback(progress) {
+          setRestoreProgress(progress);
+        },
+      });
+    }, [mx, setRestoreProgress])
   );
 };
