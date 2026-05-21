395b7af1b70d325d1144fc2077dae7a136d74c48
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-21 20:24:06 +0800

    Add files via upload

--- src/app/hooks/useRestoreBackupOnVerification.ts
+++ src/app/hooks/useRestoreBackupOnVerification.ts
@@ -1,40 +1,24 @@
 import { useSetAtom } from 'jotai';
-import { useCallback, useRef } from 'react';
+import { useCallback } from 'react';
 import { backupRestoreProgressAtom } from '../state/backupRestore';
 import { useMatrixClient } from './useMatrixClient';
 import { useKeyBackupDecryptionKeyCached } from './useKeyBackup';
-import { retryDecryptLoadedTimelines } from '../utils/keyBackup';
 
 export const useRestoreBackupOnVerification = () => {
   const setRestoreProgress = useSetAtom(backupRestoreProgressAtom);
 
   const mx = useMatrixClient();
-  const restorePromiseRef = useRef<Promise<void>>();
 
   useKeyBackupDecryptionKeyCached(
     useCallback(() => {
-      if (restorePromiseRef.current) {
-        return;
-      }
-
       const crypto = mx.getCrypto();
-      if (!crypto) {
-        return;
-      }
+      if (!crypto) return;
 
-      restorePromiseRef.current = crypto
-        .restoreKeyBackup({
-          progressCallback(progress) {
-            setRestoreProgress(progress);
-          },
-        })
-        .then(() => {
-          void retryDecryptLoadedTimelines(mx);
-        })
-        .catch(() => undefined)
-        .finally(() => {
-          restorePromiseRef.current = undefined;
-        });
+      crypto.restoreKeyBackup({
+        progressCallback(progress) {
+          setRestoreProgress(progress);
+        },
+      });
     }, [mx, setRestoreProgress])
   );
 };
