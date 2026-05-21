5e79e47414461516e4bc9b65f9c173e3a785f2e8
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-21 17:07:03 +0800

    Add files via upload

--- src/app/utils/keyBackup.ts
+++ src/app/utils/keyBackup.ts
@@ -1,9 +1,42 @@
-import { MatrixClient } from 'matrix-js-sdk';
+import { Direction, EventTimeline, MatrixClient } from 'matrix-js-sdk';
 import { ImportRoomKeyProgressData } from 'matrix-js-sdk/lib/crypto-api';
 import { decryptAllTimelineEvent } from './room';
 
 type RestoreKeyBackupOptions = {
   progressCallback?: (progress: ImportRoomKeyProgressData) => void;
+};
+
+export const retryDecryptLoadedTimelines = async (mx: MatrixClient): Promise<void> => {
+  const getFirstLinkedTimeline = (
+    timeline: EventTimeline,
+    direction: Direction
+  ): EventTimeline => {
+    const linkedTimeline = timeline.getNeighbouringTimeline(direction);
+    if (!linkedTimeline) return timeline;
+    return getFirstLinkedTimeline(linkedTimeline, direction);
+  };
+
+  const getLinkedTimelines = (timeline: EventTimeline): EventTimeline[] => {
+    const firstTimeline = getFirstLinkedTimeline(timeline, Direction.Backward);
+    const timelines: EventTimeline[] = [];
+
+    for (
+      let nextTimeline: EventTimeline | null = firstTimeline;
+      nextTimeline;
+      nextTimeline = nextTimeline.getNeighbouringTimeline(Direction.Forward)
+    ) {
+      timelines.push(nextTimeline);
+    }
+
+    return timelines;
+  };
+
+  await Promise.allSettled(
+    mx
+      .getRooms()
+      .flatMap((room) => getLinkedTimelines(room.getUnfilteredTimelineSet().getLiveTimeline()))
+      .map((timeline) => decryptAllTimelineEvent(mx, timeline))
+  );
 };
 
 export const restoreKeyBackupAndDecrypt = async (
@@ -19,7 +52,5 @@ export const restoreKeyBackupAndDecrypt = async (
     progressCallback: options.progressCallback,
   });
 
-  await Promise.allSettled(
-    mx.getRooms().map((room) => decryptAllTimelineEvent(mx, room.getLiveTimeline()))
-  );
+  await retryDecryptLoadedTimelines(mx);
 };
