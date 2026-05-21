56a8a74433074c51e04ca3eca8122ca3bf6c0e38
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-21 15:21:13 +0800

    Add files via upload

--- src/app/hooks/useRestoreBackupOnVerification.ts
+++ src/app/hooks/useRestoreBackupOnVerification.ts
@@ -1,24 +1,53 @@
 import { useSetAtom } from 'jotai';
-import { useCallback } from 'react';
-import { backupRestoreProgressAtom } from '../state/backupRestore';
+import { useCallback, useRef } from 'react';
+import {
+  BackupProgressStatus,
+  backupRestoreProgressAtom,
+  setBackupRestoreProgressAtom,
+} from '../state/backupRestore';
 import { useMatrixClient } from './useMatrixClient';
 import { useKeyBackupDecryptionKeyCached } from './useKeyBackup';
 
 export const useRestoreBackupOnVerification = () => {
   const setRestoreProgress = useSetAtom(backupRestoreProgressAtom);
+  const setRestoreProgressState = useSetAtom(setBackupRestoreProgressAtom);
 
   const mx = useMatrixClient();
+  const restorePromiseRef = useRef<Promise<void>>();
 
   useKeyBackupDecryptionKeyCached(
     useCallback(() => {
-      const crypto = mx.getCrypto();
-      if (!crypto) return;
+      if (restorePromiseRef.current) {
+        return;
+      }
 
-      crypto.restoreKeyBackup({
-        progressCallback(progress) {
-          setRestoreProgress(progress);
-        },
-      });
-    }, [mx, setRestoreProgress])
+      const crypto = mx.getCrypto();
+      if (!crypto) {
+        setRestoreProgressState({
+          status: BackupProgressStatus.Idle,
+        });
+        return;
+      }
+
+      restorePromiseRef.current = crypto
+        .restoreKeyBackup({
+          progressCallback(progress) {
+            setRestoreProgress(progress);
+          },
+        })
+        .then(() => {
+          setRestoreProgressState({
+            status: BackupProgressStatus.Done,
+          });
+        })
+        .catch(() => {
+          setRestoreProgressState({
+            status: BackupProgressStatus.Idle,
+          });
+        })
+        .finally(() => {
+          restorePromiseRef.current = undefined;
+        });
+    }, [mx, setRestoreProgress, setRestoreProgressState])
   );
 };
