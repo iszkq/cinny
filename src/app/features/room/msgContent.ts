2d8a8c170fc78d0c01ef2bcf3b238f057c6cec30
iszkq <167393945+iszkq@users.noreply.github.com>
2026-05-25 16:40:38 +0800

    Add files via upload

--- src/app/features/room/msgContent.ts
+++ src/app/features/room/msgContent.ts
@@ -18,8 +18,8 @@ import { TUploadItem } from '../../state/room/roomInputDrafts';
 import { encodeBlurHash } from '../../utils/blurHash';
 import { scaleYDimension } from '../../utils/common';
 
-const IMAGE_PREVIEW_WIDTH = 420;
-const IMAGE_PREVIEW_HEIGHT = 720;
+const IMAGE_PREVIEW_WIDTH = 560;
+const IMAGE_PREVIEW_HEIGHT = 1120;
 
 const generateThumbnailContent = async (
   mx: MatrixClient,
